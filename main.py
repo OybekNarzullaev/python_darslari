7c743e27de124693fd167529f3eaa436e21c3255
OybekNarzullaev <oybeknarzullaev99@gmail.com>
2021-06-30 09:03:51 +0500

    whileda else ni qo'llanilishi

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("salom")
+# loop lar (takrorlabuvchilar)
+# else ni while da qo'llanilishi
+
+i = 0
+while i <= 5:
+    i += 1
+    print(i)
+else:
+    print(f"i ning oxirgi qiymati -> {i}")
+
+
